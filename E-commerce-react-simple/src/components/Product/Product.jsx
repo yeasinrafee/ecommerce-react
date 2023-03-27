fdd65c5edcc6ff0ab09e69123a149abe7719e5ac
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-03-28 01:23:49 +0600

    Product component

---
+++ E-commerce-react-simple/src/components/Product/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    const {img, name, price, ratings, seller} = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <h6>{name}</h6>
+        </div>
+    );
+};
+
+export default Product;
---
+++ E-commerce-react-simple/src/components/Shop/Shop.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
+import Product from '../Product/Product';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() =>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    })
+
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                {
+                    products.map(product => <Product
+                    key={product.id}
+                    product = {product}
+                    >
+                    </Product>
+                    )
+                }
+            </div>
+            <div className="cart-container">
+                <h2>Order Summary</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
