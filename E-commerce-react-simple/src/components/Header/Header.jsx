8b700ae031e2fd109252d3934d7207386a2bb283
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-03-27 15:35:01 +0600

    Navbar added

--- E-commerce-react-simple/src/components/Header/Header.jsx
+++ E-commerce-react-simple/src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is a Header</h1>
-        </div>
+        <nav className='heading'>
+            <img src={logo} alt="Logo" />
+            <div className='link-box'>
+                <a href="/order">Order</a>
+                <a href="/review">Order Review</a>
+                <a href="/inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
     );
 };
 
