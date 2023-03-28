ce050427c43550ad0476fa66ac2a7198e90b75f1
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-03-28 09:26:57 +0600

    Adding cards details

--- E-commerce-react-simple/src/components/Shop/Shop.jsx
+++ E-commerce-react-simple/src/components/Shop/Shop.jsx
@@ -4,6 +4,7 @@ import Product from '../Product/Product';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() =>{
         fetch('products.json')
@@ -12,7 +13,8 @@ const Shop = () => {
     })
 
     const handleAddToCart = (product) =>{
-        console.log(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
     }
 
     return (
@@ -30,6 +32,7 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <h2>Order Summary</h2>
+                <p>Cart size: {cart.length}</p>
             </div>
         </div>
     );
