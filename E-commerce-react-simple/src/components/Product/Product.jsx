058a5bad38e7985cceff804a64f9a62c9e1bd3a3
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-03-28 09:07:18 +0600

    Products added to ui

--- E-commerce-react-simple/src/components/Product/Product.jsx
+++ E-commerce-react-simple/src/components/Product/Product.jsx
@@ -6,7 +6,13 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
+            <div className="product-info">
+                <h6 className='cart-heading'>{name}</h6>
+                <p className='product-price'>Price ${price}</p>
+                <p className='product-seller'>Manufacturer: {seller}</p>
+                <p className='product-rating'>Rating: {ratings} star</p>
+            </div>
+            <button className="btn-cart">Add to cart</button>
         </div>
     );
 };
