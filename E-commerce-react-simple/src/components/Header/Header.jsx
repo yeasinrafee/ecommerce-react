4c723b59191d06bb2726cf7b47a39b2084fd2e45
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-07 18:08:49 +0600

    Adding react router for multiple components

--- E-commerce-react-simple/src/components/Header/Header.jsx
+++ E-commerce-react-simple/src/components/Header/Header.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import './Header.css';
-import logo from '../../images/Logo.svg';
+import React from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <nav className='heading'>
-            <img src={logo} alt="Logo" />
-            <div className='link-box'>
-                <a href="/order">Order</a>
-                <a href="/review">Order Review</a>
-                <a href="/inventory">Manage Inventory</a>
-                <a href="/login">Login</a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="heading">
+      <img src={logo} alt="Logo" />
+      <div className="link-box">
+        <Link to="/">Shop</Link>
+        <Link to="/orders">Orders</Link>
+        <Link to="/inventory">Manage Inventory</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- E-commerce-react-simple/src/main.jsx
+++ E-commerce-react-simple/src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Shop from "./components/Shop/Shop";
+import Home from "./components/Layout/Home";
+import Orders from "./components/Orders/Orders";
+import Inventory from "./components/Inventory/Inventory";
+import Login from "./components/Login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/",
+        element: <Shop />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+      {
+        path: "inventory",
+        element: <Inventory />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
