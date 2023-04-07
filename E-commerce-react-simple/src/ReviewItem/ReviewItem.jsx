4a7380fe7e124dd10642d945db9d6992f6df11ac
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-07 22:46:39 +0600

    Showing all cart items in order section

--- E-commerce-react-simple/src/ReviewItem/ReviewItem.jsx
+++ E-commerce-react-simple/src/ReviewItem/ReviewItem.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import "./ReviewItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 const ReviewItem = ({ product }) => {
+  const { id, img, price, name, quantity } = product;
   return (
-    <div>
-      <h2>review</h2>
+    <div className="review-item">
+      <img src={img} alt="" />
+      <div className="review-details">
+        <p className="product-title">{name}</p>
+        <p>
+          Price: <span className="orange-text">{price}</span>
+        </p>
+        <p>
+          Order Quantity: <span className="orange-text">{quantity}</span>
+        </p>
+      </div>
+      <button className="btn-delete">
+        <FontAwesomeIcon className="delete-icon" icon={faTrashAlt} />
+      </button>
     </div>
   );
 };
--- E-commerce-react-simple/src/components/Orders/Orders.jsx
+++ E-commerce-react-simple/src/components/Orders/Orders.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
 import ReviewItem from "../../ReviewItem/ReviewItem";
+import "./Orders.css";
 
 const Orders = () => {
   const cart = useLoaderData();
   return (
     <div className="shop-container">
-      <div className="product-container">
+      <div className="review-container">
         {cart.map((product) => (
           <ReviewItem key={product.id} product={product} />
         ))}
